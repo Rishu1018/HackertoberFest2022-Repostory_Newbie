06d69be671f529d6e4e8dd26cdbd0cc7cec7be05
Nishant Shaklan <90901154+shaklan2001@users.noreply.github.com>
2022-10-05 18:00:07 +0530

    Add files via upload

---
+++ Bubble_sort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+
+
+
+    int counter = 0;
+    while (counter < n)
+    {
+        for (int i = 0; i < n - counter; i++)
+        {
+            if (arr[i - 1] > arr[i])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i - 1];
+                arr[i - 1] = temp;
+            }
+        }
+        counter++;
+    }
+
+
+
+
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i];
+    }
+    cout<<endl;
+
+    return 0;
+}
